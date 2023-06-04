6467cdbab33ad83e27b5110f131214b676b0cc5a
Yash Godara <yash17godara@gmail.com>
2023-06-04 08:17:32 +0530

    😁 changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 import logo from "../assets/images/icons8-movie-100.png";
 
+const links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Blog",
+    path: "/",
+  },
+  {
+    name: "About US",
+    path: "/",
+  },
+  {
+    name: "Contact US",
+    path: "/",
+  },
+]
+
 export default function NavBar() {
   const [navbar, setNavbar] = useState(false);
 
   return (
     <>
-      <nav className="w-full z-10 bg-[#0c111b] shadow shadow-zinc-800 h-[80px] ease-in-out delay-1000 ">
+      <nav className="w-full z-10 bg-[#0c111b] shadow shadow-zinc-800 ease-in-out delay-1000 ">
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
@@ -15,61 +34,65 @@ export default function NavBar() {
               </a>
               {/* Menu */}
               <div className="md:hidden">
-                <button
-                  className="p-2 text-white rounded-md outline-none"
-                  onClick={() => setNavbar(!navbar)}
-                >
-                  {navbar ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      />
-                    </svg>
-                  )}
-                </button>
+                <div className="flex flex-row items-center gap-2">
+                  {/* Button */}
+                  <button
+                    className="p-2 text-white rounded-md outline-none"
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    {navbar ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    )}
+                  </button>
+                  {/* Avt */}
+                  <img
+                    src={"https://tecdn.b-cdn.net/img/new/avatars/2.webp"}
+                    className="w-10 rounded-full"
+                    alt="Avatar"
+                  />
+                </div>
               </div>
+
             </div>
           </div>
-          <div>
+          <div className="md:flex flex-row md:gap-4  items-center">
             {/* Large Devices */}
             <div
               className={`hidden flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0`}
             >
               <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                <li className="text-zinc-200  hover:text-blue-600">
-                  <a href="/">Home</a>
-                </li>
-                <li className="text-zinc-200  hover:text-blue-600">
-                  <a href="/">Blog</a>
-                </li>
-                <li className="text-zinc-200  hover:text-blue-600">
-                  <a href="/">About US</a>
-                </li>
-                <li className="text-zinc-200  hover:text-blue-600">
-                  <a href="/">Contact US</a>
-                </li>
+                {/* Map */}
+                {links.map((link) => (
+                  <li className="text-zinc-200  hover:text-blue-600">
+                    <a href={link.path}>{link.name}</a>
+                  </li>
+                ))}
               </ul>
             </div>
             {/* Mobile */}
@@ -83,20 +106,22 @@ export default function NavBar() {
                 </a>
               </div>
               <ul className="items-center px-2 pt-5 justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                <li className="text-gray-200 hover:text-blue-600">
-                  <a href="/">Home</a>
-                </li>
-                <li className="text-gray-200 hover:text-blue-600">
-                  <a href="/">Blog</a>
-                </li>
-                <li className="text-gray-200 hover:text-blue-600">
-                  <a href="/">About US</a>
-                </li>
-                <li className="text-gray-200 hover:text-blue-600">
-                  <a href="/">Contact US</a>
-                </li>
+                {/* Map */}
+                {links.map((link) => (
+                  <li className="text-zinc-200  hover:text-blue-600">
+                    <a href={link.path}>{link.name}</a>
+                  </li>
+                ))}
               </ul>
             </div>
+            <div>
+              <img
+                src={"https://tecdn.b-cdn.net/img/new/avatars/2.webp"}
+                className="hidden md:flex w-10 rounded-full"
+                alt="Avatar"
+              />
+            </div>
+
             {/* Mobile End */}
           </div>
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,106 +1,124 @@
 import { useState } from "react";
+import logo from "../assets/images/icons8-movie-100.png";
 
 export default function NavBar() {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className="w-full bg-purple-500 shadow">
-      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-        <div>
-          <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <a href="javascript:void(0)">
-              <h2 className="text-2xl font-bold text-white">LOGO</h2>
-            </a>
-            <div className="md:hidden">
-              <button
-                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                onClick={() => setNavbar(!navbar)}
-              >
-                {navbar ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
+    <>
+      <nav className="w-full z-10 bg-[#0c111b] shadow shadow-zinc-800 h-[80px] ease-in-out delay-1000 ">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+              <a href="/">
+                <img src={logo} className="h-[60px]" />
+              </a>
+              {/* Menu */}
+              <div className="md:hidden">
+                <div className="flex flex-row items-center gap-2">
+                  {/* Button */}
+                  <button
+                    className="p-2 text-white rounded-md outline-none"
+                    onClick={() => setNavbar(!navbar)}
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                )}
-              </button>
+                    {navbar ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-6 h-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    )}
+                  </button>
+                  {/* Avt */}
+                  <img
+                    src="https://tecdn.b-cdn.net/img/new/avatars/2.webp"
+                    className="w-10 rounded-full"
+                    alt="Avatar"
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <div
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-              navbar ? "block" : "hidden"
-            }`}
-          >
-            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-              <li className="text-white hover:text-indigo-200">
-                <a href="javascript:void(0)">Home</a>
-              </li>
-              <li className="text-white hover:text-indigo-200">
-                <a href="javascript:void(0)">Blog</a>
-              </li>
-              <li className="text-white hover:text-indigo-200">
-                <a href="javascript:void(0)">About US</a>
-              </li>
-              <li className="text-white hover:text-indigo-200">
-                <a href="javascript:void(0)">Contact US</a>
-              </li>
-            </ul>
-
-            <div className="mt-3 space-y-2 lg:hidden md:inline-block">
-              <a
-                href="javascript:void(0)"
-                className="inline-block w-full px-4 py-2 text-center text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
-              >
-                Sign in
-              </a>
-              <a
-                href="javascript:void(0)"
-                className="inline-block w-full px-4 py-2 text-center text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
-              >
-                Sign up
-              </a>
+          <div>
+            {/* Large Devices */}
+            <div
+              className={`hidden flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0`}
+            >
+              <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                <li className="text-zinc-200  hover:text-blue-600">
+                  <a href="/">Home</a>
+                </li>
+                <li className="text-zinc-200  hover:text-blue-600">
+                  <a href="/">Blog</a>
+                </li>
+                <li className="text-zinc-200  hover:text-blue-600">
+                  <a href="/">About US</a>
+                </li>
+                <li className="text-zinc-200  hover:text-blue-600">
+                  <a href="/">Contact US</a>
+                </li>
+              </ul>
             </div>
+            {/* Mobile */}
+            <div
+              className={`flex-1 z-50 fixed shad border-r border-zinc-800 bg-[#0f1014] top-0 bottom-0  left-0 px-4 justify-center w-[50%] h-screen md:hidden md:pb-0 md:mt-0 
+            ${navbar ? "block" : "hidden"}`}
+            >
+              <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                <a href="/">
+                  <img src={logo} className="h-[60px]" />
+                </a>
+              </div>
+              <ul className="items-center px-2 pt-5 justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                <li className="text-gray-200 hover:text-blue-600">
+                  <a href="/">Home</a>
+                </li>
+                <li className="text-gray-200 hover:text-blue-600">
+                  <a href="/">Blog</a>
+                </li>
+                <li className="text-gray-200 hover:text-blue-600">
+                  <a href="/">About US</a>
+                </li>
+                <li className="text-gray-200 hover:text-blue-600">
+                  <a href="/">Contact US</a>
+                </li>
+              </ul>
+            </div>
+            {/* Mobile End */}
           </div>
         </div>
-        <div className="hidden space-x-2 md:inline-block">
-          <a
-            href="javascript:void(0)"
-            className="px-4 py-2 text-white bg-gray-600 rounded-md shadow hover:bg-gray-800"
-          >
-            Sign in
-          </a>
-          <a
-            href="javascript:void(0)"
-            className="px-4 py-2 text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
-          >
-            Sign up
-          </a>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
+
+// <NavLink
+//   to="/"
+//   className={({ isActive }) =>
+//     isActive ? "bg-green-500 font-bold" : "bg-red-500 font-thin"
+//   }
+// >
+//   Users
+// </NavLink>
