47a7a7a9410c97f2e6d95d936d202e2b33caf14e
Yash Godara <yash17godara@gmail.com>
2023-06-06 16:40:52 +0530

    😑 Slider Trending | 😑 API Changes

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef } from "react";
 import GlobalApi from "../services/api/GlobalApi";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
-interface Movie {
+interface Trending {
     id: number;
     title: string;
     poster_path: string;
@@ -11,29 +11,27 @@ interface Movie {
 
 function Slider() {
     // State
-    const [trendingMovies, setTrendingMovies] = useState<Movie[]>([]);
+    const [trending, setTrending] = useState<Trending[]>([]);
 
-    // Fetch Trending Movies API
+    // Trending API
     useEffect(() => {
         let isMounted = true;
-        const getTrendingMovies = async () => {
+        const getTrending = async () => {
             try {
-                const response = await GlobalApi.getTrendingMovies;
+                const response = await GlobalApi.getTrending();
                 if (isMounted) {
                     // Get Data and Slice it
                     const data = response.data.results;
                     const slicedData = data.slice(0, 10);
-                    // log data
-                    console.log(slicedData);
-                    // State
-                    setTrendingMovies(slicedData);
+                    // console.log(slicedData);
+                    setTrending(slicedData);
                 }
             } catch (error) {
                 console.error(error);
             }
         };
-        // Fetch Trending Movies API
-        getTrendingMovies();
+        // Fetch Trending API
+        getTrending();
         return () => {
             isMounted = false;
         };
@@ -50,14 +48,11 @@ function Slider() {
             element.scrollLeft += windowWidth - 110;
         }
     };
-
     const slideRight = (element: HTMLDivElement | null) => {
         if (element) {
             element.scrollLeft -= windowWidth - 110;
         }
     };
-
-
 
     return (
         <>
@@ -68,14 +63,15 @@ function Slider() {
                 onClick={() => slideLeft(elementRef.current)}
                 className="hidden  md:block right-0 cursor-pointer text-white text-[30px] absolute mx-8 md:mt-[150px] xl:mt-[275px]" />
 
+            {/* Slider */}
             <div
                 ref={elementRef}
                 className="flex w-full overflow-x-auto px-2 md:px-16  py-4 scroll-smooth scrollbar-none">
-                {trendingMovies.map((movie, index) => (
+                {trending.map((data, index) => (
 
-                    <img key={index} src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                    <img key={index} src={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`}
                         className="min-w-full md:h-[310px] xl:h-[550px] object-cover object-left-top mr-5 rounded-md md:hover:border-[2px] border-zinc-600 translate-all duration-75 ease-in-out"
-                        alt={movie.title} />
+                        alt={data.title} />
 
                 ))}
             </div >
--- src/services/api/GlobalApi.tsx
+++ src/services/api/GlobalApi.tsx
@@ -1,19 +1,29 @@
 import axios from 'axios';
-
 // Global API Config
 import { API_URL, API_KEY } from './config.json';
 const APP_API_URL = API_URL;
 const APP_API_KEY = API_KEY;
 
+//Fetch Trending API
+const getTrending = () => {
+    const response = axios.get(`${APP_API_URL}/trending/all/day?api_key=${APP_API_KEY}&language=en-US`);
+    return response;
+};
 
 // Fetch Trending Movies API
-const getTrendingMovies = axios.get(`${APP_API_URL}/trending/tv/day?api_key=${APP_API_KEY}&language=en-US`);
+const getTrendingMovies = async () => {
+    const response = await axios.get(`${APP_API_URL}/trending/movie/day?api_key=${APP_API_KEY}&language=en-US`);
+    return response;
+};
 
-// Fetch Trending Series API
-const getTrendingSeries = axios.get(`${APP_API_URL}/trending/tv/day?api_key=${APP_API_KEY}&language=en-US`);
+// Fetch Trending TV Shows API
+const getTrendingTVShows = async () => {
+    const response = await axios.get(`${APP_API_URL}/trending/tv/day?api_key=${APP_API_KEY}&language=en-US`);
+    return response;
+};
 
 export default {
+    getTrending,
     getTrendingMovies,
-    getTrendingSeries,
-    
-}
+    getTrendingTVShows,
+};
